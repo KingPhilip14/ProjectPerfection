5935943eb81051aa463bb01f9da9ff0a1b2ec084
KingPhilip14 <kingian404@gmail.com>
2023-03-28 16:05:05 -0500

    Shop Features
    Fully implemented the shop features. Players can now properly buy and sell items.

--- src/Battle/HealingItem.java
+++ src/Battle/HealingItem.java
@@ -29,7 +29,7 @@ public class HealingItem extends Item
         salePrice = (int)Math.round(price * 0.75);
     }
     
-    public HealingItem(String name, String description)
+    public HealingItem(String name, String description, int price)
     {
         super.name = name;
         super.description = description;
--- src/Exploration/Location.java
+++ src/Exploration/Location.java
@@ -128,4 +128,45 @@ public abstract class Location extends GameProperty
         
         MainGame.clearScreen();
     }
+    
+    protected void findChest()
+    {
+        if(hasFullChest() && !chest.isAlreadyFound())
+        {
+            openChest();
+        }
+        else if(hasFullChest() && chest.isAlreadyFound())
+        {
+            int chance = new Random().nextInt(2);
+            
+            switch(chance)
+            {
+                case 0:
+                    openChest();
+                    break;
+                case 1:
+                    MainGame.printlnlnWait("You couldn't find a chest...", 25, 2000);
+                    break;
+            }
+        }
+        else
+        {
+            MainGame.printlnlnWait("You couldn't find a chest...", 25, 2000);
+        }
+        
+        MainGame.promptToEnter();
+    }
+    
+    private boolean hasFullChest()
+    {
+        return !chest.isEmpty();
+    }
+    
+    private void openChest()
+    {
+        System.out.println("");
+        MainGame.printlnln("You found a chest! What does it have?", 25);
+        chest.open();
+        MainGame.wait(1000);
+    }
 }
